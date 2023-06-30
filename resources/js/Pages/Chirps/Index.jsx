716c5c49690028fc9fedb1fa34c2e23e05802aad
Ford Henley <fordp86@gmail.com>
2023-06-30 17:25:23 -0500

    Completed Project
    Completed Project

--- resources/js/Pages/Chirps/Index.jsx
+++ resources/js/Pages/Chirps/Index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import Chirp from "@/Components/Chirp";
 import InputError from "@/Components/InputError";
 import PrimaryButton from "@/Components/PrimaryButton";
 import { useForm, Head } from "@inertiajs/react";
 
-export default function Index({ auth }) {
+export default function Index(props) {
     const { data, setData, post, processing, reset, errors } = useForm({
         message: "",
     });
@@ -15,9 +16,8 @@ export default function Index({ auth }) {
     };
 
     return (
-        <AuthenticatedLayout user={auth.user}>
+        <AuthenticatedLayout auth={props.auth} user={props.auth.user}>
             <Head title="Chirps" />
-
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <form onSubmit={submit}>
                     <textarea
@@ -31,6 +31,12 @@ export default function Index({ auth }) {
                         Chirp
                     </PrimaryButton>
                 </form>
+
+                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    {props.chirps.map((chirp) => (
+                        <Chirp key={chirp.id} chirp={chirp} />
+                    ))}
+                </div>
             </div>
         </AuthenticatedLayout>
     );
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -29,14 +29,14 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store'])
-    ->middleware(['auth', 'verified']);
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
